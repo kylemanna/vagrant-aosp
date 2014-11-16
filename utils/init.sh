bf7659881debd9b5bc90a85a371623c3a94d8768
Kyle Manna <kyle@kylemanna.com>
2014-11-16 10:51:07 -0800

    utils: Unset SSH_AUTH_SOCK to workaround net-ssh bug
    * Since net-ssh will likely continue to be broken in popular
      distributions for some time, use a harmless workaround.

--- utils/init.sh
+++ utils/init.sh
@@ -2,6 +2,15 @@
 #
 # Automate the annoying process of creating a vagrant VM and resizing it.
 #
+
+# Vagrant (net-ssh) blows up for people using ed25519 keys loaded into an
+# ssh-agent, references:
+# https://github.com/mitchellh/vagrant/issues/4367
+# https://github.com/net-ssh/net-ssh/pull/162
+#
+# Workaround:
+unset SSH_AUTH_SOCK
+
 SIZE=${SIZE:-204800}
 set -ex
 vagrant up
